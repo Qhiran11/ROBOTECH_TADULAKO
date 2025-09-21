1b12b5bc473736b50a7fcc2464ad3ae750a34d96
Qhiran11 <muhammadqhiran@gmail.com>
2025-09-21 13:17:22 +0800

    IRIS

--- app/Models/Berita.php
+++ app/Models/Berita.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Berita extends Model
 {
-    //
+    use HasFactory;
+
+    // TAMBAHKAN BARIS INI
+    protected $table = 'berita';
+
+    protected $fillable = [
+        'judul',
+        'slug',
+        'konten',
+        'gambar',
+    ];
 }
